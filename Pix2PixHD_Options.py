05af1d2ef46b066517b79c3d6a4e8a31e0d29120
Hyun-Jin Jeong <jeong_hj@khu.ac.kr>
2020-12-02 11:34:55 +0900

    Update Pix2PixHD_Options.py

--- Pix2PixHD_Options.py
+++ Pix2PixHD_Options.py
@@ -46,6 +46,9 @@ class BaseOption(object):
         self.parser.add_argument('--saturation_lower_limit_target', type=float, default=-1, help="Saturation value (lower limit) of target")
         self.parser.add_argument('--saturation_upper_limit_target', type=float, default=1, help="Saturation value (upper limit) of target")
         
+        self.parser.add_argument('--saturation_clip_input', type=bool, default=True, help="Saturation clip for input data")
+        self.parser.add_argument('--saturation_clip_target', type=bool, default=True, help="Saturation clip for target data")
+
         #----------------------------------------------------------------------
 
         # data augmentation
